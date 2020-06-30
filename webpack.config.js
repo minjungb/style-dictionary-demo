4ccec77f7997f509346105ce37f17dcd6d0effdb
Min <minjungbin@gmail.com>
2020-06-29 21:02:45 -0400

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry:"./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname,"dist")
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    node: {
+        fs:"empty"
+    },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: ["css-loader"]
+            }
+        ]
+    }
+}
